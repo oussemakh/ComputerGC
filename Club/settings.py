8e2136a8d8e9f6ad01ca2cfcdbd2d931fd9aa905
unknown <oussemakhlifi35@outlook.fr>
2018-10-21 15:59:19 +0100

    ffff5

--- Club/settings.py
+++ Club/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p-3rsiq+p-f)5%cpyrm#jo961n+0izru%e77cfd#&70%ry7uba'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecsclub.herokuapp.com']
+ALLOWED_HOSTS = ['ecsclub.herokuapp.com','127.0.0.1']
 
 
 # Application definition
