eced7b29d023c2fd985af1362482599ca190b820
unknown <oussemakhlifi35@outlook.fr>
2018-10-29 23:25:10 +0100

    postgresheylllllll

--- Club/settings.py
+++ Club/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -91,7 +93,7 @@ DATABASES = {
         'NAME': 'CGC',
         'USER': 'postgres',
         'PASSWORD': '123',
-        'HOST': 'localhost',
+        'HOST': 'eccgc.herokuapp.com',
         'PORT': '5432',
     }
 }
