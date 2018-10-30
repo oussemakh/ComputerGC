15053a6f3b420f533dddeb07224b0f35d61fcfae
unknown <oussemakhlifi35@outlook.fr>
2018-10-30 15:00:35 +0100

    postgrespleasework

--- Club/settings.py
+++ Club/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,7 +93,7 @@ WSGI_APPLICATION = 'Club.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'h.sqlite3'),
     }
 }
 import dj_database_url
@@ -153,3 +158,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'koussema35@gmail.com'
 EMAIL_HOST_PASSWORD = 'OUSSEMAcss123456789'
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
