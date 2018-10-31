251878f0edc1a5991ff0d3019df935f4265e750b
unknown <oussemakhlifi35@outlook.fr>
2018-10-31 18:46:04 +0100

    trying to make it work

--- Club/settings.py
+++ Club/settings.py
@@ -136,14 +136,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static','media-root')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'live-static','static-root')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
